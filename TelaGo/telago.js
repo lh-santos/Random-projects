d70e93fdac0f61a0b3fddca6cffb150aaa78fd8d
Luis Henrique Silva Santos <luissantosus2@gmail.com>
2026-08-27 23:22:56 -0300

    Mudanças de cores

---
+++ TelaGo/telago.js
@@ -0,0 +1,30 @@
+        var header = document.getElementById('header');
+        var navitelago_header = document.getElementById('navitelago_header');
+        var content = document.getElementById('content');
+        var showSidebar = false;
+
+        function togglesidebar() { /*alterna para a barra lateral*/
+            showSidebar = !showSidebar;
+            if(showSidebar) {
+                navitelago_header.style.marginLeft = '-10vw';
+                navitelago_header.style.animationName = 'showSidebar';
+                content.style.filter = 'blur(2px)';
+            } else {
+                navitelago_header.style.marginLeft = '-100vw';
+                navitelago_header.style.animationName = '';
+                content.style.filter = '';
+            }
+        }
+
+        function closeSidebar() {
+            if (showSidebar) {
+                togglesidebar(); /*Fecha o sidebar quando tocado em uma area fora dele*/
+            }
+        }
+
+        window.addEventListener('resize', function(event) {
+            if (window.innerWidth > 768 && showSidebar) {
+                togglesidebar();
+            }
+        }
+    )
